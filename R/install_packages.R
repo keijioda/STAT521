e800ae46cfe565bdd9e7667be747f44da9ac91bc
Keiji Oda <keiji.oda@gmail.com>
2021-09-07 10:58:52 -0700

    Added R packages to be used

---
+++ R/install_packages.R
@@ -0,0 +1,4 @@
+
+# STAT 521: R packages to be used
+pacs <- c("tidyverse", "tigerstats", "ABACUS", "TeachingDemos", "binom", "emmeans", "mvtnorm", "pwr")
+install.packages(pacs, repos = "http://cloud.r-project.org")
